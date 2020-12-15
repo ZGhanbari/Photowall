001c0deb19d359970d135cb963366c0e22efbb74
ZGhanbari <z.ghanbari14@gmail.com>
2020-12-15 21:27:05 +0330

    Updates

---
+++ precache-manifest.1a319253d65665c9ef92fd41859efc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d362858eb6460d42b36feac490fa7",
+    "url": "/Photowall/index.html"
+  },
+  {
+    "revision": "d48443fca63f25c15e94",
+    "url": "/Photowall/static/css/main.30413583.chunk.css"
+  },
+  {
+    "revision": "72ea2a725127ddcb5ccb",
+    "url": "/Photowall/static/js/2.d2096a06.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Photowall/static/js/2.d2096a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48443fca63f25c15e94",
+    "url": "/Photowall/static/js/main.c4d395de.chunk.js"
+  },
+  {
+    "revision": "5313af39fa507ec3e138",
+    "url": "/Photowall/static/js/runtime-main.8cdd9682.js"
+  }
+]);
